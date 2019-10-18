9ae2da86464392a7ef019c547f36845a140286bf
Mikhail_Gorbachenko <michael.gorbachenko@yandex.ru>
2019-10-18 16:25:44 +0300

    what i am doing? IDK

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -2,11 +2,18 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import java.util.Collections;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
