21bd485d2f843932274cc1749009fce1d435f13f
Mikhail_Gorbachenko <michael.gorbachenko@yandex.ru>
2019-10-20 16:01:41 +0300

    produces trouble

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,56 +1,63 @@
 package hello;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
-@RequestMapping(value = "/greeting" , produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+@RequestMapping(value = "/greeting", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
 public class GreetingController {
 
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @GetMapping(path="/hrest")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
+    @GetMapping(path = "/hrest")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
 
-    @PostMapping(path="/hrest")
-    public Greeting post(){
+    @PostMapping(path = "/hrest")
+    public Greeting post() {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, "post"));
     }
 
-    @PutMapping(path="/hrest")
-    public Greeting put(){
+    @PutMapping(path = "/hrest")
+    public Greeting put() {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, "put"));
     }
 
-    @DeleteMapping(path="/hrest")
-    public Greeting delete(){
+    @DeleteMapping(path = "/hrest")
+    public Greeting delete() {
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, "delete"));
     }
 
 
-    @GetMapping(path="/users/{userID}" )
-    public String getUser(@PathVariable String userID){
+    @GetMapping(path = "/users/{userID}")
+    public String getUser(@PathVariable String userID) {
         return "Call user info with ID = " + userID;
     }
 
-    @GetMapping(path="/users")
-    public String getUsers(@RequestParam(value = "page" , defaultValue = "0") int page,
-                                 @RequestParam(value = "limit", defaultValue = "10") int limit,
-                           @RequestParam(value = "Hug-A-Duck", required = false) boolean hug){
+    @GetMapping(path = "/users")
+    public String getUsers(@RequestParam(value = "page", defaultValue = "0") int page,
+                           @RequestParam(value = "limit", defaultValue = "10") int limit,
+                           @RequestParam(value = "Hug-A-Duck", required = false) boolean hug) {
 
-        StringBuilder returnValue = new StringBuilder("Page " + page + " Limit " + limit  + "Hug a Duck " + hug + "\n");
+        StringBuilder returnValue = new StringBuilder("Page " + page + " Limit " + limit + " Hug a Duck " + hug + "\n");
 
         for (int i = 0; i < limit; i++) {
             returnValue.append("User id ").append(i).append("\n");
         }
 
         return returnValue.toString();
+    }
+
+    @GetMapping(path = "/response")
+    public ResponseEntity getResponse() {
+        return new ResponseEntity(HttpStatus.OK);
     }
 
 
