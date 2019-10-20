60c0a14f0129d8d084f478dcd05ec5690755d4b0
Mikhail_Gorbachenko <michael.gorbachenko@yandex.ru>
2019-10-20 14:17:15 +0300

    Different @Mapping for existed class

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -1,20 +1,36 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
     private static final String TEMPLATE = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
+    @GetMapping
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name){
         return new Greeting(counter.incrementAndGet(), String.format(TEMPLATE, name));
     }
+
+    @PostMapping
+    public String post(){
+        return "Post";
+    }
+
+    @PutMapping
+    public String put(){
+        return "put";
+    }
+
+    @DeleteMapping
+    public String delete(){
+        return "Delete";
+    }
+
+
 
 }
